557b68bd2da1ad10bd62e32c1141586d036c9920
KarolDiniz <dinizkaroline923@gmail.com>
2023-05-19 20:00:50 -0300

    implementando deletarUser e CreateUser

--- src/main/java/com/example/firebase/util/Connection.java
+++ src/main/java/com/example/firebase/util/Connection.java
@@ -15,15 +15,20 @@ import java.util.concurrent.ExecutionException;
 @Repository
 public class Connection {
 
+    public Connection(){
+        initializeFirebase();
+    }
+
     @PostConstruct
     private void initializeFirebase() {
         try {
+
             FileInputStream serviceAccount =
-                    new FileInputStream("./serviceAccount.json");
+                    new FileInputStream("serviceAccount.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://bdii-firebase-default-rtdb.firebaseio.com")
+                    .setDatabaseUrl("https://bdii-firebase-45aa0-default-rtdb.firebaseio.com")
                     .build();
 
             FirebaseApp.initializeApp(options);
@@ -33,7 +38,7 @@ public class Connection {
         }
     }
 
-    public void createNewUser(User user) throws InterruptedException, ExecutionException {
+    public void createNewUser(User user)  {
 
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getId());
         // Grava os dados do objeto User no Firebase Realtime Database
@@ -48,21 +53,22 @@ public class Connection {
         // Aguarda a conclusão da gravação
         try {
             writeFuture.get(); // Bloqueia a execução até que a gravação seja concluída ou falhe
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException err) {
             // Lida com exceção, se ocorrer algum erro na gravação
-            throw e;
+            System.err.println(err.getMessage());
         }
     }
 
     public User returnUser(String key) {
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("users").child(key);
-        User user = null;
+        CompletableFuture<Void> writeFuture = new CompletableFuture<>();
+        final User[] user = {null};
         databaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     // Converte o DataSnapshot para o objeto User
-                    user = dataSnapshot.getValue(User.class);
+                    user[0] = dataSnapshot.getValue(User.class);
                 }
             }
             @Override
@@ -71,21 +77,32 @@ public class Connection {
                 databaseError.toException().printStackTrace();
             }
         });
-        return user;
+        return user[0];
     }
 
     public void deleteUser(String key) {
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("users").child(key);
 
-        databaseRef.removeValue(new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                if (databaseError == null) {
-                    System.out.println("Dado excluído com sucesso no Firebase");
-                } else {
-                    System.out.println("Erro ao excluir o dado no Firebase: " + databaseError.getMessage());
-                }
+        CompletableFuture<Void> writeFuture = new CompletableFuture<>();
+        databaseRef.removeValue((error, ref) -> {
+            if (error == null) {
+                writeFuture.complete(null); // Marca a gravação como concluída
+            } else {
+                writeFuture.completeExceptionally(error.toException()); // Marca a gravação como falha
             }
         });
+        // Aguarda a conclusão da gravação
+        try {
+            writeFuture.get(); // Bloqueia a execução até que a gravação seja concluída ou falhe
+        } catch (InterruptedException | ExecutionException err) {
+            // Lida com exceção, se ocorrer algum erro na gravação
+            System.err.println(err.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection connection = new Connection();
+       // User user = new User("1","Ellen","Ellencassiamatos@gmail.com");
+        connection.deleteUser("1");
     }
 }
